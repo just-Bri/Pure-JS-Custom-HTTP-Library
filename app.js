51a6e21d487213746967f5b21b8e1e7c5a07cd9f
Bridger <bridgerrhammond@gmail.com>
2019-02-28 13:56:37 -0600

    Post, put, and delete

--- app.js
+++ app.js
@@ -5,6 +5,58 @@ http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts) {
   if (err) {
     console.log(err);
   } else {
-    console.log(err, posts);
+    // console.log(posts);
+  }
+});
+
+// Get single post
+http.get("https://jsonplaceholder.typicode.com/posts/1", function(err, post) {
+  if (err) {
+    console.log(err);
+  } else {
+    // console.log(post);
+  }
+});
+
+// Post request
+// Create data
+const data = {
+  title: "Custom post",
+  body: "This is a custom  post"
+};
+
+// Create Post
+http.post("https://jsonplaceholder.typicode.com/posts", data, function(
+  err,
+  post
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    // console.log(post);
+  }
+});
+
+// Update Post
+http.put("https://jsonplaceholder.typicode.com/posts/1", data, function(
+  err,
+  post
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    // console.log(post);
+  }
+});
+
+// Delete post
+http.del("https://jsonplaceholder.typicode.com/posts/1", function(
+  err,
+  response
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(response);
   }
 });
